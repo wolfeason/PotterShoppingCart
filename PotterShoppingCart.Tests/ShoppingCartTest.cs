f304bdfc89e894a538e77c36a319050801adeed5
Eason <Eason@DESKTOP-7B4OB8N>
2017-01-18 23:18:24 +0800

    (紅燈)第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -14,14 +14,29 @@ namespace PotterShoppingCart.Tests
         private List<CarItem> _Items = new List<CarItem>();
 
         [TestMethod()]
-        public void hoppingCartTest_Volume_1_Should_100()
+        public void ShoppingCartTest_Volume_1_Should_100()
         {
+            //第一集買了一本，其他都沒買，價格應為100*1=100元
             //Arrange
             var expected = 100;
-            var ShoppingCart = new ShoppingCart();
+            var shoppingcart = new ShoppingCart();
             _Items.Add(new CarItem { Volume = 1, Name = "第一冊", SellPrice = 100, Quantity = 1 });
             //Act
-            var actual = ShoppingCart.CalculateShoppingCart(_Items);
+            var actual = shoppingcart.CalculateShoppingCart(_Items);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ShoppingCartTest_Volume_1_Volume_2_Should_190()
+        {
+            //第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190
+            var expected = 190;
+            var shoppingcart = new ShoppingCart();
+            _Items.Add(new CarItem { Volume = 1, Name = "第一冊", SellPrice = 100, Quantity = 1 });
+            _Items.Add(new CarItem { Volume = 2, Name = "第二冊", SellPrice = 100, Quantity = 1 });
+            //Act
+            var actual = shoppingcart.CalculateShoppingCart(_Items);
             //Assert
             Assert.AreEqual(expected, actual);
         }

9c4610b6e0dd7517f0e0cdcd3ef51e1104337944
Eason <Eason@DESKTOP-7B4OB8N>
2017-01-19 00:20:50 +0800

    (紅燈)一二三四集各買了一本，價格應為100*4*0.8=320

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -27,6 +27,7 @@ namespace PotterShoppingCart.Tests
             Assert.AreEqual(expected, actual);
         }
 
+
         [TestMethod()]
         public void ShoppingCartTest_Volume_1_Volume_2_Should_190()
         {
@@ -51,6 +52,23 @@ namespace PotterShoppingCart.Tests
             _Items.Add(new CarItem { Volume = 1, Name = "第一冊", SellPrice = 100, Quantity = 1 });
             _Items.Add(new CarItem { Volume = 2, Name = "第二冊", SellPrice = 100, Quantity = 1 });
             _Items.Add(new CarItem { Volume = 3, Name = "第三冊", SellPrice = 100, Quantity = 1 });
+            //Act
+            var actual = shoppingcart.CalculateShoppingCart(_Items);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void ShoppingCartTest_Volume_1_Volume_2_Volume_3_Volume_4_Should_320()
+        {
+            //一二三四集各買了一本，價格應為100*4*0.8=320
+            var expected = 320;
+            var shoppingcart = new ShoppingCart();
+            _Items.Add(new CarItem { Volume = 1, Name = "第一冊", SellPrice = 100, Quantity = 1 });
+            _Items.Add(new CarItem { Volume = 2, Name = "第二冊", SellPrice = 100, Quantity = 1 });
+            _Items.Add(new CarItem { Volume = 3, Name = "第三冊", SellPrice = 100, Quantity = 1 });
+            _Items.Add(new CarItem { Volume = 4, Name = "第四冊", SellPrice = 100, Quantity = 1 });
             //Act
             var actual = shoppingcart.CalculateShoppingCart(_Items);
             //Assert

61f834c03e79648a041070572920c75d0e420133
Eason <Eason@DESKTOP-7B4OB8N>
2017-01-18 22:56:24 +0800

    加入專案檔案。

---
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PotterShoppingCart;
+
+namespace PotterShoppingCart.Tests
+{
+
+    [TestClass()]
+    public class ShoppingCartTest
+    {
+        private List<CarItem> _Items = new List<CarItem>();
+
+        [TestMethod()]
+        public void hoppingCartTest_Volume_1_Should_100()
+        {
+            //Arrange
+            var expected = 100;
+            var ShoppingCart = new ShoppingCart();
+            _Items.Add(new CarItem { Volume = 1, Name = "第一冊", SellPrice = 100, Quantity = 1 });
+            //Act
+            var actual = ShoppingCart.CalculateShoppingCart(_Items);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

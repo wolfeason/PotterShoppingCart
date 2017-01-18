dde575ab919f8474e4c95ce4d0d79a74cc5f3a57
Eason <Eason@DESKTOP-7B4OB8N>
2017-01-18 22:59:16 +0800

    (綠燈) 第一集買了一本，其他都沒買，價格應為100*1=100元

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -8,5 +8,18 @@ namespace PotterShoppingCart
 {
     public class ShoppingCart
     {
+        public int CalculateShoppingCart(IEnumerable<CarItem> _Items)
+        {
+            int results1 = _Items.Sum(x => x.SellPrice * x.Quantity);
+            return results1;
+        }
+    }
+
+    public class CarItem
+    {
+        public int Volume { get; set; }
+        public string Name { get; set; }
+        public int SellPrice { get; set; }
+        public int Quantity { get; set; }
     }
 }
